7c381b34d5fc3138f2c07b16b14bce75bafd0670
lgrando12 <oohhmypicle@gmail.com>
2025-11-03 22:19:03 -0300

    feat: Adiciona Helm Chart final e todas as correções da pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_container_registry" "acr" {
   name                = "acrunyleyalucasgrando" # <--- NOME ÚNICO GLOBALMENTE
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
+  sku                 = "Standard"
   admin_enabled       = true
 }
 
